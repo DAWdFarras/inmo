37419330042b4e7ac8e2fc726939009aa3d10f91
dfarras <diegofarrasguevara@gmail.com>
2019-02-22 15:15:49 +0100

    First master commit

---
+++ src/main/java/com/daw/inmo/InmoApplication.java
@@ -0,0 +1,13 @@
+package com.daw.inmo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InmoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InmoApplication.class, args);
+	}
+
+}
